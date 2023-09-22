f11e7f7787745dc561dc9032c174abffa85ab614
junheiLee <116637090+junheiLee@users.noreply.github.com>
2023-09-22 12:46:47 +0900

    Revert "feat: 회원가입 아이디 중복 확인 기능"

--- src/main/java/com/murang/rental/data/repository/UserRepository.java
+++ src/main/java/com/murang/rental/data/repository/UserRepository.java
@@ -2,7 +2,5 @@ package com.murang.rental.data.repository;
 
 import com.murang.rental.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,String> { }
--- src/main/java/com/murang/rental/service/UserService.java
+++ src/main/java/com/murang/rental/service/UserService.java
@@ -1,12 +1,9 @@
 package com.murang.rental.service;
 
 
-import com.murang.rental.data.dto.UserDto;
 import com.murang.rental.data.entity.User;
 
 public interface UserService {
 
-     void insertUser(UserDto userDto);
-     boolean isDuplicate(String id);
-
+    User insertUser(User user);
 }
